4ddc9ec242cdb69124b82a14dc8cb3ee6d0bf96c
divya-250406 <divyanagavalli888@gmail.com>
2026-08-06 10:32:25 +0530

    Add files via upload

---
+++ DataTypes/A2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+
+        int alphabets = 0;
+        int digits = 0;
+
+        foreach (char ch in str)
+        {
+            if (char.IsLetter(ch))
+                alphabets++;
+            else if (char.IsDigit(ch))
+                digits++;
+        }
+
+        Console.WriteLine("Number of alphabets = " + alphabets);
+        Console.WriteLine("Number of digits = " + digits);
+    }
+}
