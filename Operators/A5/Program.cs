ca16b46046ac5d28bc0240e56a2ec5de5c0359d9
divya-250406 <divyanagavalli888@gmail.com>
2026-08-06 10:36:43 +0530

    Add files via upload

---
+++ Operators/A5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1, num2, temp;
+
+        Console.Write("Enter first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nOriginal Values:");
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+
+        // 1. Pre-increment
+        num2 = ++num1;
+        Console.WriteLine("\nAfter Pre-Increment:");
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+
+        // Reset values
+        Console.Write("\nEnter first number again: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number again: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        // 2. Post-increment
+        num2 = num1++;
+        Console.WriteLine("\nAfter Post-Increment:");
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+
+        // 3. Swap values
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        Console.WriteLine("\nAfter Swapping:");
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+    }
+}
