4dbe55536e7b207a31cf9111e74fc00350485927
divya-250406 <divyanagavalli888@gmail.com>
2026-08-06 10:50:56 +0530

    Add files via upload

---
+++ Methods and Properties/A12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class RandomHelper
+{
+    static Random random = new Random();
+
+    public static int randint(int min, int max)
+    {
+        return random.Next(min, max + 1);
+    }
+
+    public static double randdouble(double min, double max)
+    {
+        return min + random.NextDouble() * (max - min);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Random Integer: " + RandomHelper.randint(1, 10));
+
+        Console.WriteLine("Random Double: " + RandomHelper.randdouble(1, 10));
+    }
+}
+
