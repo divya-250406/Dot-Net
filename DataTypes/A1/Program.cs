fa590eb154ef9be98bffd562989e0ee63ba2edad
divya-250406 <divyanagavalli888@gmail.com>
2026-08-06 10:22:36 +0530

    Add files via upload

---
+++ DataTypes/A1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the side of the square: ");
+        int side=Convert.ToInt32(Console.ReadLine());
+        int area=side*side;
+        Console.WriteLine("Area of the square="+area);
+    }
+}
