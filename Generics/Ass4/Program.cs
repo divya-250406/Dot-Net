0af4a18a1e80bfb17e827fd689ac7498a4a5bbda
divya-250406 <divyanagavalli888@gmail.com>
2026-08-06 11:14:48 +0530

    Add files via upload

---
+++ Generics/Ass4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlphaDigitList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> AlphaList=new List<char>();
+            List<char> DigitList=new List<char>();
+            Console.Write("Enter a string: ");
+            string input=Console.ReadLine();
+            foreach(char ch in input)
+            {
+                if(char.IsLetter(ch))
+                {
+                    AlphaList.Add(ch);
+                }
+                else if(char.IsDigit(ch))
+                {
+                    DigitList.Add(ch);
+                }
+            }
+            AlphaList.Sort();
+            DigitList.Sort();
+            Console.WriteLine("\nAlphabets in Sorted Order:");
+            foreach(char c in AlphaList)
+            {
+                Console.Write(c+" ");
+            }
+            Console.WriteLine("\n\nDigits in Sorted Order:");
+            foreach(char c in DigitList)
+            {
+                Console.Write(c+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
