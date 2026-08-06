b517629013336588886a89a28904b9034b99dcff
divya-250406 <divyanagavalli888@gmail.com>
2026-08-06 10:12:58 +0530

    Add files via upload

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+
 class Program
 {
     static void Main()
     {
-        Console.Write("Enter the side of the square: ");
-        int side=Convert.ToInt32(Console.ReadLine());
-        int area=side*side;
-        Console.WriteLine("Area of the square="+area);
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+
+        int alphabets = 0;
+        int digits = 0;
+
+        foreach (char ch in str)
+        {
+            if (char.IsLetter(ch))
+                alphabets++;
+            else if (char.IsDigit(ch))
+                digits++;
+        }
+
+        Console.WriteLine("Number of alphabets = " + alphabets);
+        Console.WriteLine("Number of digits = " + digits);
     }
 }
