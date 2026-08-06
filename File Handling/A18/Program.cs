30fe67bda0fd6208a1c8ca009ca31ecd7c3eee81
divya-250406 <divyanagavalli888@gmail.com>
2026-08-06 11:06:03 +0530

    Add files via upload

---
+++ File Handling/A18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath="StudentDetails.txt";
+        Console.Write("Enter Student Name: ");
+        string name=Console.ReadLine();
+        Console.Write("Enter Roll Number: ");
+        string rollNo=Console.ReadLine();
+        Console.Write("Enter Department: ");
+        string department=Console.ReadLine();
+        StreamWriter writer=new StreamWriter(filePath);
+        writer.WriteLine("Student Details");
+        writer.WriteLine("----------------------");
+        writer.WriteLine("Name: "+name);
+        writer.WriteLine("Roll No: "+rollNo);
+        writer.WriteLine("Department: "+department);
+        writer.Close();
+        Console.WriteLine();
+        Console.WriteLine("File created successfully.");
+        Console.WriteLine("Data has been written, saved and the file is closed.");
+    }
+}
