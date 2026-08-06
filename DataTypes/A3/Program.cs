2bd9702a3f84d52fcb8fea70c4398ff0ca97d1d4
divya-250406 <divyanagavalli888@gmail.com>
2026-08-06 10:34:13 +0530

    Add files via upload

---
+++ DataTypes/A3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+
+        string result = "";
+
+        foreach (char ch in str)
+        {
+            char newChar = (char)(ch + 1);
+
+            if (char.IsUpper(newChar))
+                result += char.ToLower(newChar);
+            else if (char.IsLower(newChar))
+                result += char.ToUpper(newChar);
+            else
+                result += newChar;
+        }
+
+        Console.WriteLine("Modified string = " + result);
+    }
+}
